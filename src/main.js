1558443b5c3a14d3e27593c7a291284c40a769a7
Hank <hanksan@Hanks-iMac.local>
2025-12-18 16:55:23 +0000

    fix svelte 5 mount

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import "./app.css";
+import { mount } from "svelte";
 import App from "./App.svelte";
 
-const app = new App({
+mount(App, {
   target: document.getElementById("app")
 });
-
-export default app;
