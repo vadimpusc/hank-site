ae0b18aa0ae9aa5bd95168c18d998674a7d65396
Hank <hanksan@Hanks-iMac.local>
2026-01-20 11:13:17 +0000

    film

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,23 @@
 <script>
-  import Header from "./components/Header.svelte";
-  import Hero from "./components/Hero.svelte";
-  import About from "./components/About.svelte";
+ 
+
+
   import Filmography from "./components/Filmography.svelte";
-  import Contact from "./components/Contact.svelte";
-  import Footer from "./components/Footer.svelte";
+
+ 
 </script>
 
 <div class="page">
-  <Header />
+
 
   <main>
-    <section id="home">
-      <Hero />
-    </section>
-
-    <section id="about">
-      <About />
-    </section>
 
     <section id="filmography">
       <Filmography />
     </section>
 
-    <section id="contact">
-      <Contact />
-    </section>
+
   </main>
 
-  <Footer />
+
 </div>
